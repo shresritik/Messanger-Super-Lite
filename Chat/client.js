3fe769b7e7101307fd9a89657cf0d1e3ac262cab
Ritik <shrestharitik@gmail.com>
2020-08-17 20:07:38 +0545

    Chat app v1.0

---
+++ Chat/client.js
@@ -0,0 +1,60 @@
+const socket = io('http://localhost:8000');
+
+let msgtxt = document.getElementById('text')
+let submit = document.getElementById('form')
+let messageContainer = document.getElementsByClassName('container')[0]
+
+let append = (message, position) => {
+
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    console.log(messageElement)
+    messageContainer.appendChild(messageElement);
+}
+
+
+const name = prompt('Enter your name');
+socket.emit('new-user-joined', name);
+socket.on('user-joined', name => {
+
+    append(`${name} has joined`, 'right')
+});
+socket.on('receive', data => {
+    const feedback = document.getElementsByClassName('feedback')[0]
+    feedback.innerText=''
+    append(`${name}:${data.message}`, 'left')
+
+});
+socket.on('left', name => {
+    append(`${name} left the chat`, 'right');
+    console.log('${name} has left')
+});
+
+msgtxt.addEventListener('keypress', function (name) {
+
+    socket.emit('typing', name)
+    console.log('key is pressed by ${name}')
+});
+
+socket.on('typing', data => {
+    const feedback = document.getElementsByClassName('feedback')[0]
+    feedback.innertext=''
+
+console.log(name)
+    feedback.innerText= `${name} is typing`
+    console.log(feedback)
+
+});
+submit.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const message = msgtxt.value;
+    
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    
+    msgtxt.value = '';
+  
+   
+})
