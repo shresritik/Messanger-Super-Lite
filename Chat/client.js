0b10dd7f77c50f0d07e6bbee180945198789ea60
Ritik <shrestharitik@gmail.com>
2020-08-19 19:39:41 +0545

    commits

--- Chat/client.js
+++ Chat/client.js
@@ -1,5 +1,4 @@
-// const socket = io('http://localhost:8000');
-const socket = io('https://fierce-earth-28305.herokuapp.com/ ');
+const socket = io('http://localhost:8000');
 
 let msgtxt = document.getElementById('text')
 let submit = document.getElementById('form')
